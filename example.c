3cf4480eb77ff6587c72019e53c08b42e81d9872
manfredhod <74592878+manfredhod@users.noreply.github.com>
2020-11-17 13:40:46 +0100

    Update example.c

--- example.c
+++ example.c
@@ -6,31 +6,34 @@
 #define MINUTE 60
 
 int main (void){
-    int bus;
-    float speed, time, minutes, seconds;
+    int bus, minutes, seconds, time;
+    float speed;
+    char strings_plural[2][8] = {"minutes", "seconds"};
+    char strings_singular[2][8] = {"minute", "second"};
     printf("Enter speed (km/h): ");
     scanf(" %f", &speed);
     speed /= 3.6;
     printf("Converted to m/s. You entered: %2.3f\n", speed);
 
-    time = (float)distance / speed;
-    minutes = time / (float)MINUTE;
+    time = (int)distance / speed;
+    minutes = time / (int)MINUTE;
     seconds = fmod(time, MINUTE);
 
-    if(minutes > 1){
-         printf("The travel time is: %4.1f minutes", minutes);
+    if (minutes == 0){
+        printf("The travel time is: %d %s.\n", seconds,
+        (seconds > 1) ? strings_plural[1] : strings_singular[1]);
     }
+
+    else if (seconds == 0){
+        printf("The travel time is: %d %s. \n", minutes, 
+        (minutes > 1) ? strings_plural[0] : strings_singular[0]);
+    }
+
+    
     else{
-        printf("The travel time is: 0 minutes");
-    }
-    if(seconds == 1.0){
-        printf(" and 1 second");
-    }
-    else if(seconds > 1){
-         printf(" and %4.1f seconds", seconds);
-    }
-    else{
-        printf(" and 0 seconds");
+        printf("The travel time is: %d %s and %d %s. \n", 
+        minutes, (minutes > 1) ? strings_plural[0] : strings_singular[0],
+        seconds, (seconds > 1) ? strings_plural[1] : strings_singular[1]);
     }
     return EXIT_SUCCESS;
 }   
