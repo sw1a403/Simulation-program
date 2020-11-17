f777c5bcadd68d713e3dd39ccafb5b728f54fa50
manfredhod <74592878+manfredhod@users.noreply.github.com>
2020-11-17 13:10:04 +0100

    Update example.c

--- example.c
+++ example.c
@@ -3,16 +3,37 @@
 #include <math.h>
 #include <time.h>
 #define distance 1000
+#define MINUTE 60
 
 int main (void){
     int bus;
-    float speed, time;
+    float speed, time, minutes, seconds;
     printf("Enter speed (km/h): ");
     scanf(" %f", &speed);
     speed /= 3.6;
     printf("Converted to m/s. You entered: %2.3f\n", speed);
-    time = (float)distance / speed;
-    printf("The travel time is : %4.1f seconds.\n", time);
 
+    time = (float)distance / speed;
+    minutes = time / (float)MINUTE;
+    seconds = fmod(time, MINUTE);
+
+    if(minutes == 1){
+        printf("The travel time is: 1 minute");
+    }
+    else if(minutes > 1){
+         printf("The travel time is: %4.1f minute", minutes);
+    }
+    else{
+        printf("The travel time is: 0 minutes");
+    }
+    if(seconds == 60){
+        printf(" and 1 second");
+    }
+    else if(seconds > 1){
+         printf(" and %4.1f seconds", seconds);
+    }
+    else{
+        printf(" and 0 seconds");
+    }
     return EXIT_SUCCESS;
 }   
