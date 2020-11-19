91be959bdfee14bb8f25239dff390eaa2b1d7625
Frydensbjerg <48320816+Frydensbjerg@users.noreply.github.com>
2020-11-19 10:33:20 +0100

    Update example.c

--- example.c
+++ example.c
@@ -8,6 +8,7 @@ void simulate_one_bus();
 void speed_vehicle(float *vehicle_speed);
 void traffic_light(int *temp_interval);
 void calculate_travel_time(int light_interval, float speed, int dif_dist_intersec[6]);
+int light_green_or_red(int total_time, int light_interval);
 
 int main (void){
     simulate_one_bus(); 
@@ -45,21 +46,51 @@ void traffic_light(int *temp_interval){
         *temp_interval = temp;
     }
     else if(answer == 'n')
-        printf("\nTraffic lights will not be implemented");
+        printf("\nTraffic lights will not be implemented\n");
 }
 
 void calculate_travel_time(int light_interval, float speed, int dif_dist_intersec[6]){
-    int minutes, seconds, time, i, distance, total_time = 0, total_distance = 0;
+    int time, round, distance, total_time = 0, total_distance = 0, 
+        time_added_round = 0, total_time_added = 0;
+
+
+    for(round = 0; round <= 5; round++){
+        distance = dif_dist_intersec[i];
+        time = (int)distance / speed;
+        total_time += time;
+        time_added_round = light_green_or_red(total_time, light_interval); 
+        total_time_added += time_added_round;
+        print_time_intersec(time, total_time, time_added_round, total_time_added, round);
+        
+    }
+    total_time += total_time_added;
+    print_time_intersec(time, total_time, time_added_round, total_time_added, round);
+}
+
+int light_green_or_red(int total_time, int light_interval){
+    int time_until_change = total_time % light_interval, 
+        count = 0, time_added, i;
+    for(i = 0; i < total_time; i += light_interval)
+        count++;
+    if(count % 2 == 1)
+        time_added = 0;
+    else{
+        if(time_until_change == 0)
+            time_added = 1;
+        else
+            time_added = light_interval - time_until_change;
+    }
+    return time_added;
+}
+
+void print_time_intersec(int time, int total_time, int time_added_round, int total_time_added, int round){
+    int minutes, seconds;
     char strings_plural[2][8] = {"minutes", "seconds"};
     char strings_singular[2][8] = {"minute", "second"};
 
-    for(i = 0; i <= 5; i++){
-        distance = dif_dist_intersec[i];
-        total_distance += distance;
-        time = (int)distance / speed;
-        total_time += time;
-        minutes = (int)time / MINUTE;
-        seconds = (int)fmod(time, MINUTE);     
+    minutes = (int)time / MINUTE;
+    seconds = (int)fmod(time, MINUTE);
+    if(round <= 5){
         if(minutes == 0){
             printf("The bus reached intersection [%d] after %d %s.\n", (i + 1), seconds,
             (seconds > 1) ? strings_plural[1] : strings_singular[1]);
@@ -71,19 +102,47 @@ void calculate_travel_time(int light_interval, float speed, int dif_dist_interse
             minutes, (minutes > 1) ? strings_plural[0] : strings_singular[0],
             seconds, (seconds > 1) ? strings_plural[1] : strings_singular[1]);
         }
-    }
-    total_time = (int)total_distance / speed;
-    minutes = (int)total_time / MINUTE;
-    seconds = (int)fmod(total_time, MINUTE);     
-    if(minutes == 0){
-        printf("The bus reached the end of the course after %d %s.\n", seconds,
-        (seconds > 1) ? strings_plural[1] : strings_singular[1]);
-    } else if(seconds == 0){
-        printf("The bus reached the end of the course after %d %s.\n", minutes, 
-        (minutes > 1) ? strings_plural[0] : strings_singular[0]);
-    } else{
-        printf("The bus reached the end of the course after %d %s and %d %s.\n", 
-        minutes, (minutes > 1) ? strings_plural[0] : strings_singular[0],
-        seconds, (seconds > 1) ? strings_plural[1] : strings_singular[1]);
+        minutes = (int)time_added_round / MINUTE;
+        seconds = (int)fmod(time_added_round, MINUTE);
+        if(time_added_round > 0){
+            if(minutes == 0){
+                printf("At intersection [%d] there were red light and the bus waited for %d %s.\n", (i + 1), seconds,
+                (seconds > 1) ? strings_plural[1] : strings_singular[1]);
+            } else if(seconds == 0){
+                printf("At intersection [%d] there were red light and the bus waited for %d %s.\n", (i + 1), minutes, 
+                (minutes > 1) ? strings_plural[0] : strings_singular[0]);
+            } else{
+                printf("At intersection [%d] there were red light and the bus waited for %d %s and %d %s.\n", (i + 1),
+                minutes, (minutes > 1) ? strings_plural[0] : strings_singular[0],
+                seconds, (seconds > 1) ? strings_plural[1] : strings_singular[1]);
+            }
+        }
+    } else {
+        minutes = (int)total_time / MINUTE;
+        seconds = (int)fmod(total_time, MINUTE);     
+        if(minutes == 0){
+            printf("The bus reached the end of the course after %d %s.\n", seconds,
+            (seconds > 1) ? strings_plural[1] : strings_singular[1]);
+        } else if(seconds == 0){
+            printf("The bus reached the end of the course after %d %s.\n", minutes, 
+            (minutes > 1) ? strings_plural[0] : strings_singular[0]);
+        } else{
+            printf("The bus reached the end of the course after %d %s and %d %s.\n", 
+            minutes, (minutes > 1) ? strings_plural[0] : strings_singular[0],
+            seconds, (seconds > 1) ? strings_plural[1] : strings_singular[1]);
+        }
+        minutes = (int)total_time_added / MINUTE;
+        seconds = (int)fmod(total_time_added, MINUTE);
+        if(minutes == 0){
+            printf("Total time spend at the light were %d %s.\n", seconds,
+            (seconds > 1) ? strings_plural[1] : strings_singular[1]);
+        } else if(seconds == 0){
+            printf("Total time spend at the light were %d %s.\n",  minutes, 
+            (minutes > 1) ? strings_plural[0] : strings_singular[0]);
+        } else{
+            printf("Total time spend at the light were %d %s and %d %s.\n",
+            minutes, (minutes > 1) ? strings_plural[0] : strings_singular[0],
+            seconds, (seconds > 1) ? strings_plural[1] : strings_singular[1]);
+        }
     }
 }
